21a7a876056ef5903194bfacf87dd09bea24b04f
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-23 13:16:41 -0800

    Added src folder

---
+++ C++ Crash Course/Crash_Course_src/Crash_Course_src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Crash_Course_src
+//
+//  Created by Rohan Singh on 12/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
