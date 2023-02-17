84b136e8a5e335f10451ece2809810182de348ea
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-17 14:42:14 -0500

    Added Helper functions to NN

--- Machine_Learning/Linear Regression/single_output_lr.cpp
+++ Machine_Learning/Linear Regression/single_output_lr.cpp
@@ -6,32 +6,40 @@
 
 using namespace std;
 
+
+//  Class for Neural Networks
 class NeuralNetwork{
     
+    // Instance Fields
     private:
-    int* weights;
-    int dim;
 
+
+    double* weights;
+    int dim;
+    double epsilon;
+
+    // Methods
     public:
     
-    NeuralNetwork(int dimensions){
+    NeuralNetwork(int dimensions, double eps){
         this->dim = dimensions;
-        weights = new int[this->dim];
-
+        weights = new double[this->dim];
+        this->epsilon = eps;
     }
 
-    int get_NN_output(int vec[]){
+    int get_NN_output(double vec[]){
+        double output = 0;
         for(int i = 0; i < dim; i++){
-
+            output+=(this->weights[i])*(vec[i]);
         }
-        return 0;
+        return output;
     }
 
     int get_dimensions(){
         return this->dim;
     }
 
-    int* get_weights(){
+    double* get_weights(){
         return this->weights;
     }
 
@@ -43,14 +51,51 @@ class NeuralNetwork{
         cout<<"\n";
     }
 
+    void update_weights(double gradient[]){
+        for(int i = 0; i < this->dim; i++)
+            this->weights[i]-=(this->epsilon*gradient[i]);
+    }
+
+    void set_weights(double* temp_weight){
+        this->weights = temp_weight;
+    }
+
 
 };
 
+
+//  Testing method
+void test();
+
+
 int main(){
 
-    NeuralNetwork* NN = new NeuralNetwork(10);
+    test();
+    
+    return 0;
+}
+
+
+void test(){
+
+    NeuralNetwork* NN = new NeuralNetwork(10,0.1);
 
     NN->print_weights();
 
-    return 0;
+    double temp[] = {1.0,1.0,1.0,2.0,3.0,1.0,1.0,1.0,2.0,3.0};
+
+    NN->set_weights(temp);
+
+    NN->print_weights();
+
+    double input[] = {1.0,1.0,1.0,2.0,3.0,1.0,1.0,1.0,2.0,3.0};
+
+    cout<<NN->get_NN_output(input);
+
+    cout<<"\n";
+
+    NN->update_weights(input);
+
+    NN->print_weights();
+
 }
