eb83c0478f665d394982d70baf58319e4a09a3c9
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-08 12:02:04 -0500

    Added Template code for the Tape problem

---
+++ Algorithms/Greedy/Tape.cpp
@@ -0,0 +1,34 @@
+//  Author: Rohan Singh
+//  Greedy Algorithm to solve he Tape recorder problem
+//  3/8/2023
+
+
+//  Imports
+#include <iostream>
+
+using namespace std;
+
+
+//  Using greedy algorithm 1
+int* algo_1(int t[], int x[], int n){
+    return NULL;
+}
+
+
+//  Using greedy algorithm 2
+int* algo_2(int t[], int x[], int n){
+    return NULL;
+}
+
+
+//  Tester function to check the time taken for the recordings
+int test_time(int t[], int x[], int n){
+    return 0;
+}
+
+
+//  Main function
+int main(){
+
+    return 0;
+}
