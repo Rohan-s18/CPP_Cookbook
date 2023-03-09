6e5d52fd51fc1626370a32d150f2fe25db8fe92c
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-09 14:45:52 -0500

    Added Skeletal and helper code for scheduler

---
+++ Algorithms/Dynammic Programming/scheduler.cpp
@@ -0,0 +1,41 @@
+//  Author: Rohan Singh
+//  3/9/2023
+//  Dynammic Programming Problem solved using dynamic programming
+
+
+//  Imports
+#include <iostream>
+
+
+//  Method for planning the schedule
+int plan(int low_stress[], int high_stress[], char schedule[]){
+    int payoff = 0;
+    return payoff;
+}
+
+
+//  Helper Function to print out an array of chars
+void print_array_c(char arr[], int n){
+    std::cout<<"[";
+    for(int i = 0; i < n; i++){
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"]\n";
+}
+
+
+//  Helper Function to print out an array of ints
+void print_array(int arr[], int n){
+    std::cout<<"[";
+    for(int i = 0; i < n; i++){
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"]\n";
+}
+
+
+//  Main Function
+int main(){
+
+    return 0;
+}
